39d05325c4110fe1ef265651078c2ceead6149b5
Akinori Hattori <hattya@gmail.com>
2022-04-29 17:36:27 +0900

    Migrate to PEP 517

--- setup.py
+++ setup.py
@@ -37,56 +37,17 @@ class test(Command):
         unittest.main(None, argv=argv)
 
 
-try:
-    with open('README.rst') as fp:
-        long_description = fp.read()
-except OSError:
-    long_description = ''
-
-packages = ['scmver']
-package_data = {
-    'scmver': ['py.typed'],
-}
-
 cmdclass = {
     'test': test,
 }
 
-setup(name='scmver',
-      version=scmver.get_version(**{
+setup(version=scmver.get_version(**{
           'root': os.path.dirname(os.path.abspath(__file__)),
           'spec': 'micro',
           'write_to': os.path.join('scmver', '__version__.py'),
           'fallback': 'scmver:__version__',
       }),
-      description='A package version manager based on SCM tags',
-      long_description=long_description,
-      author='Akinori Hattori',
-      author_email='hattya@gmail.com',
-      url='https://github.com/hattya/scmver',
-      license='MIT',
-      packages=packages,
-      package_data=package_data,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Software Development :: Version Control',
-          'Topic :: System :: Software Distribution',
-          'Topic :: Utilities',
-      ],
-      python_requires='>= 3.7',
       cmdclass=cmdclass,
-      extras_require={
-          'cli': ['click'],
-      },
       entry_points={
           'console_scripts': [
               'scmver = scmver.cli:run [cli]',
