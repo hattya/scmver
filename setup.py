0a7b1c94c8beca9302245b7e61ed55abcc2155de
Akinori Hattori <hattya@gmail.com>
2024-03-22 22:12:53 +0900

    setup: drop test command

--- setup.py
+++ setup.py
@@ -6,42 +6,13 @@
 import os
 import sys
 
-from setuptools import setup, Command
+from setuptools import setup
 
 # ensure to load the current version
 sys.path.insert(0, os.path.dirname(__file__))
 
 import scmver
 
-
-class test(Command):
-
-    description = 'run unit tests'
-    user_options = [('failfast', 'f', 'stop on first fail or error')]
-
-    boolean_options = ['failfast']
-
-    def initialize_options(self):
-        self.failfast = False
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import unittest
-
-        # run unittest discover
-        argv = [sys.argv[0], 'discover', '--start-directory', 'tests']
-        if self.verbose:
-            argv.append('--verbose')
-        if self.failfast:
-            argv.append('--failfast')
-        unittest.main(None, argv=argv)
-
-
 setup(
     version=scmver.get_version(**scmver.load_project()),
-    cmdclass={
-        'test': test,
-    },
 )
